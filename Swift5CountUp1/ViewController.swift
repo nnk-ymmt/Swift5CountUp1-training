07958192ca7aa486ac6b61306df85d3efecb24c3
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-23 00:06:46 +0900

    first commit

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countLabel.text = "0"
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        
+        count += 1
+        countLabel.text = String(count)
+        if count >= 10 {
+            changeTextColor()
+        }
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        
+        count -= 1
+        countLabel.text = String(count)
+        if count <= 0 {
+            resetColor()
+        }
+    }
+    
+    func changeTextColor(){
+        countLabel.textColor = .yellow
+    }
+    
+    func resetColor(){
+        countLabel.textColor = .white
+    }
 }
 
